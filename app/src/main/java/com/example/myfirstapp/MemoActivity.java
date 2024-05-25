13fb5d9d8847d56c8bd59a11b211b3e51fad11f8
Ruiwpo <14218277+ruiwpo@user.noreply.gitee.com>
2024-05-25 18:36:25 +0800

    1.修改了主界面 2.新增备忘录界面 3.完成备忘录功能

---
+++ app/src/main/java/com/example/myfirstapp/MemoActivity.java
@@ -0,0 +1,50 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MemoActivity extends AppCompatActivity {
+
+    private static final String TAG = "MemoActivity";
+    EditText memo_content;
+
+    private String newMemo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo);
+
+        newMemo = getIntent().getStringExtra("memo_key");
+        memo_content = findViewById(R.id.memo_content);
+        memo_content.setText(newMemo);
+    }
+
+    public void save(View btn) {
+        //保存数据，并返回到调用页面
+        newMemo = memo_content.getText().toString();
+        Log.i(TAG, "save: " + newMemo);
+    }
+
+    public void back(View btn) {
+        //带回数据到调用页面RateActivity
+        Intent retIntent = getIntent();
+        Bundle bdl = new Bundle();
+        bdl.putString("memo_key2", newMemo);
+        retIntent.putExtras(bdl);
+
+        Log.i(TAG, "back: " + newMemo);
+
+        setResult(2, retIntent);
+
+        finish();
+    }
+}
